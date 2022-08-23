563c5303082ff3a3e09fe7285a9ed4df1095c859
anshu1016 <69161719+anshu1016@users.noreply.github.com>
2022-08-23 14:32:27 +0530

    Add files via upload

---
+++ hodor.js
@@ -0,0 +1,26 @@
+var btn = document.querySelector("#btnTranslator");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+//var serverLink = "	https://api.funtranslations.com/translate/numbers.json"
+var serverLink = "	https://api.funtranslations.com/translate/hodor.json"
+//var serverLink = "	https://api.funtranslations.com/translate/dothraki.json"
+function getTranslationLink(input){
+    return serverLink +"?" +"text=" +input
+}
+function errorHandler(error)
+{
+    console.log("Some Error has been Occoured!, Wait for SomeTime..,error")
+    alert("We will be back soon, Sorry for INconvenience..")
+}
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationLink(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        txtOutput.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+btn.addEventListener("click",clickHandler)
